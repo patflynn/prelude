d83d8cf922c6fa2eac15e04fa15d09a5eb3d4095
Patrick Flynn <big.pat@gmail.com>
2013-12-21 11:33:33 -0500

    setup go autocomplete

--- personal/emacs_custom.el
+++ personal/emacs_custom.el
@@ -12,7 +12,7 @@
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
-(prelude-require-packages '(ujelly-theme flymake-cursor flycheck))
+(prelude-require-packages '(ujelly-theme flymake-cursor flycheck company company-go))
 
 (load-theme 'ujelly t)
 
@@ -21,4 +21,34 @@
 (setq prelude-flyspell nil)
 
 (setq flymake-run-in-place nil)
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
+
+(require 'company)                                   ; load company mode
+(require 'company-go)                                ; load company mode go backend
+(setq company-tooltip-limit 20)                      ; bigger popup window
+(setq company-minimum-prefix-length 0)               ; autocomplete right after '.'
+(setq company-idle-delay .3)                         ; shorter delay before autocompletion popup
+
+;; Better company mode colors.
+(custom-set-faces
+ '(company-preview
+   ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common
+   ((t (:inherit company-preview))))
+ '(company-tooltip
+   ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-selection
+   ((t (:background "steelblue" :foreground "white"))))
+ '(company-tooltip-common
+   ((((type x)) (:inherit company-tooltip :weight bold))
+    (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection
+   ((((type x)) (:inherit company-tooltip-selection :weight bold))
+    (t (:inherit company-tooltip-selection)))))
+
 ;;; emacs_custom.el ends here
+
